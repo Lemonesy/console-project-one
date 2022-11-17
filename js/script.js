35c7dc1a4c0f221591cbb08b41aaa1d654c38a01
Дмитрий Гришин <dmitrijgrisin@MacBook-Pro-Dmitrij.local>
2022-11-18 02:27:33 +0300

    second answer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 
+if (numberOfFilms < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+  alert("Вы классический зритель");
+} else if (numberOfFilms > 30) {
+  alert("Вы киноман");
+}
+
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {
@@ -12,14 +20,23 @@ const personalMovieDB = {
   private: false
 };
 
+
+
+for (let i = 0; i < 2; i++) {
 const firstFilm = prompt('Один из просмотренных фильмов?');
 const ratingFirstFilm = prompt('На сколько вы его оцените?');
-const secondFilm = prompt('Один из просмотренных фильмов?');
-const ratingSecondFilm = prompt('На сколько вы его оцените?');
+if (firstFilm.length && ratingFirstFilm.length && firstFilm.length < 50) {
+  personalMovieDB.movies[firstFilm] = ratingFirstFilm;
+
+} else {
+  i--;
+}
+
+}
 
 
-personalMovieDB.movies[firstFilm] = ratingFirstFilm;
-personalMovieDB.movies[secondFilm] = ratingSecondFilm;
+
+
 
 console.log(personalMovieDB);
 
